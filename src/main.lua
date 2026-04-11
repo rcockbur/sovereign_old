7c32203392ac269d4a2a11b7b2efc885727ea81e
Ross <rkcockburn@gmail.com>
2026-04-10 22:56:29 -0600

    getting the blank repo into shape

---
+++ src/main.lua
@@ -0,0 +1,15 @@
+-- main.lua
+-- Love2D entry point.
+
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+end
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
